df41f3fa6d5a1e2282defd3ade12271344fc2258
Lexie Choi <lexiechoi852@gmail.com>
2023-11-28 19:18:11 -0500

    feat: updated api routes to protected routes

--- routes/api.php
+++ routes/api.php
@@ -23,24 +23,25 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout',  [UserController::class, 'logout']);
+
+    Route::apiResource('attractions', AttractionController::class);
+
+    Route::apiResource('trips', TripController::class);
+    
+    Route::get('trips/user/{id}', [TripController::class, 'getForUser']);
+    
+    Route::get('schedule-items/trip/{id}', [ScheduleItemController::class, 'getForTrip']);
+    
+    Route::apiResource('schedule-items', ScheduleItemController::class);
+    
+    Route::get('trip-items/trip/{id}', [TripItemController::class, 'getForTrip']);
+    
+    Route::apiResource('trip-items', TripItemController::class);    
+});
+
 Route::post('signup',  [UserController::class, 'store']);
 
 Route::post('login',  [UserController::class, 'login']);
-
-Route::post('logout',  [UserController::class, 'logout']);
-
-Route::apiResource('attractions', AttractionController::class);
-
-Route::apiResource('trips', TripController::class);
-
-Route::get('trips/user/{id}', [TripController::class, 'getForUser']);
-
-Route::get('schedule-items/trip/{id}', [ScheduleItemController::class, 'getForTrip']);
-
-Route::apiResource('schedule-items', ScheduleItemController::class);
-
-Route::get('trip-items/trip/{id}', [TripItemController::class, 'getForTrip']);
-
-Route::apiResource('trip-items', TripItemController::class);
-
 
