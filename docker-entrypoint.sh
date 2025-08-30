87a6e78cf404b143d9287cd30fb1780ec6d654ad
Lexie Choi <lexiechoi852@gmail.com>
2025-08-29 20:37:01 -0400

    fix: clear the database

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
 
-# Run Laravel migrations
-php artisan migrate --force
-
-# Seed the database
-php artisan db:seed --force
+php artisan migrate:fresh --seed
+## Run Laravel migrations
+#php artisan migrate --force
+#
+## Seed the database
+#php artisan db:seed --force
 
 php artisan serve --host 0.0.0.0 --port $PORT
