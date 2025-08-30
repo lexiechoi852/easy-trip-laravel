c0364e746e65900ac959d54e4dfe18b104da2309
Lexie Choi <lexiechoi852@gmail.com>
2025-08-29 20:41:11 -0400

    fix: updated entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 set -e
 
-php artisan migrate:fresh --seed
-## Run Laravel migrations
-#php artisan migrate --force
-#
-## Seed the database
-#php artisan db:seed --force
+# Run Laravel migrations
+php artisan migrate --force
 
 php artisan serve --host 0.0.0.0 --port $PORT
